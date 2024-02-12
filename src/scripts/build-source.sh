bedc46e4144e895d1b46a3d43337805a8779ed80
iliyan borisov <iliyan.borisov@ft.com>
2024-02-12 15:39:07 +0200

    Removed GO111MODULE env var

--- src/scripts/build-source.sh
+++ src/scripts/build-source.sh
@@ -2,4 +2,5 @@
 git config --global --unset url."ssh://git@github.com".insteadOf
 export GOPRIVATE="github.com/Financial-Times"
 git config --global url."https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com".insteadOf "https://github.com"
+go mod download
 go build -mod=readonly -v ./...
