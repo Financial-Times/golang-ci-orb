b432b65250ca496c9f4ceb3f0533ad54be66984d
lokendersinghft <lokender.singh@ft.com>
2025-03-05 17:27:20 +0200

    use remote docker

--- src/scripts/run-integration-tests.sh
+++ src/scripts/run-integration-tests.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker-compose -f docker-compose-tests.yml up -d --build && docker logs -f test-runner && export "TESTS_STATUS_CODE"="$(docker inspect test-runner --format="{{.State.ExitCode}}")" &&  docker-compose -f docker-compose-tests.yml down && exit "$TESTS_STATUS_CODE"
+
+docker-compose -f docker-compose-tests.yml up -d --build && docker logs -f test-runner && docker cp test-runner:"${COVERAGE_REPORT_FOLDER}" . && export "TESTS_STATUS_CODE"="$(docker inspect test-runner --format="{{.State.ExitCode}}")" &&  docker-compose -f docker-compose-tests.yml down && exit "$TESTS_STATUS_CODE"
