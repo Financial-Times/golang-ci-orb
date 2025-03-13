f7439d8878155ebfc815b9267ca0cc9a154fdfb8
lokendersinghft <lokender.singh@ft.com>
2025-03-13 17:49:50 +0200

    Kill all containers once test-runner is done

--- src/scripts/run-integration-tests.sh
+++ src/scripts/run-integration-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose -f docker-compose-tests.yaml up --abort-on-container-exit --build --exit-code-from test-runner && docker cp test-runner:"${COVERAGE_REPORT_FOLDER}" . && export "TESTS_STATUS_CODE"="$(docker inspect test-runner --format="{{.State.ExitCode}}")" && docker-compose -f docker-compose-tests.yaml down && exit "$TESTS_STATUS_CODE"
+docker-compose -f docker-compose-tests.yml up --abort-on-container-exit --build --exit-code-from test-runner && docker cp test-runner:"${COVERAGE_REPORT_FOLDER}" . && export "TESTS_STATUS_CODE"="$(docker inspect test-runner --format="{{.State.ExitCode}}")" && docker-compose -f docker-compose-tests.yml down && exit "$TESTS_STATUS_CODE"
